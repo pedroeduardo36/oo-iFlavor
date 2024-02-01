7f39a62cc792dd8efdbfff2f3a4846b9f1d76dfe
pedroeduardo36 <pedrovilarinho.gpd@gmail.com>
2024-02-01 14:49:08 -0300

    Aprofundando em propriedades

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,13 +2,13 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
     def listar_restaurantes():
         print(f"{'Nomde do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Estado")
