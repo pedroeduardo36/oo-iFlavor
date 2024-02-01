7813095c28015a5801b1851ab29acbf1e091d123
pedroeduardo36 <pedrovilarinho.gpd@gmail.com>
2024-02-01 14:57:23 -0300

    Métodos de classes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -9,21 +9,24 @@ class Restaurante:
 
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
-    
-    def listar_restaurantes():
-        print(f"{'Nomde do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Estado")
+
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f"{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Estado")
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
-        return 'Ativo ✅' if self._ativo else 'Desativo ❌'
+        return '✅' if self._ativo else '❌'
 
-
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_massa = Restaurante('Massa', 'Italiano')
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca.alternar_estado()
 
 restaurante_pizza = Restaurante('Pizza Express', 'Fast food')
 
